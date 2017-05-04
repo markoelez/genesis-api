3be8c766929cd48796992be86e5dab20ceb15b84
Marko <markoelez7@gmail.com>
2017-05-04 15:15:17 -0400

    clean

--- models/user.js
+++ models/user.js
@@ -1,3 +1,15 @@
 /**
  * Created by Marko on 5/4/17.
  */
+ var mongoose = require('mongoose');
+ var passportLocalMongoose = require('passport-local-mongoose');
+ var Schema = mongoose.Schema;
+
+ var User = new Schema({
+   username: String,
+   password: String,
+   studentID: {type: Number}
+ });
+
+   User.plugin(passportLocalMongoose);
+   module.exports = mongoose.model("User", User);
